ba4667de891ccdf296e758096e30c41f80d24d0a
Eduardo Torres <etorresbar@gmail.com>
2020-10-02 21:55:41 -0700

    Se agregaron las librerias de node y lite-server. Se crearon las clases necesarias para agregar, mostrar y eliminar contactos.

--- 15-lista/app.js
+++ 15-lista/app.js
@@ -12,23 +12,48 @@ class Contacto{
 class UI {
 
     static mostrarContacto(){
-
+        const contactos = Datos.traerContactos();
+        contactos.forEach((contacto) => UI.agregarContactoLista(contacto));
     }
 
-    static agregarContactoLista(){
+    static agregarContactoLista(contacto){
+        const lista = document.querySelector('#contacto-list');
 
+        const fila = document.createElement('tr');
+        fila.innerHTML = `
+        <td>${contacto.nombre}</td>
+        <td>${contacto.telefono}</td>
+        <td>${contacto.direccion}</td>
+        <td>${contacto.email}</td>
+        <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+        `;
+
+        lista.appendChild(fila);
     }
 
-    static eliminarContacto(){
-
+    static eliminarContacto(el){
+        if (el.classList.contains('delete')){
+            el.parentElement.parentElement.remove();
+        }
     }
 
-    static mostrarAlerta(){
+    static mostrarAlerta(mensaje, className){
+        const div = document.createElement('div');
+        div.className = `alert alter-${className}`;
+        div.appendChild(document.createTextNode(mensaje));
 
+        const container = document.querySelector('.container');
+        const form = document.querySelector('#contacto-form');
+        container.insertBefore(div, form);
+
+        setTimeout(() => document.querySelector('.alert').remove(), 3000);
     }
 
     static limpiarCampos(){
-
+        document.querySelector('#nombre').value = '';
+        document.querySelector('#direccion').value = '';
+        document.querySelector('#telefono').value = '';
+        document.querySelector('#email').value = '';
     }
 
 }
@@ -36,15 +61,59 @@ class UI {
 class Datos{
     
     static traerContactos(){
-
+        let contactos;
+        if (localStorage.getItem('contactos') === null){
+            contactos = [];
+        } else{
+            contactos = JSON.parse(localStorage.getItem('contactos'));
+        }
+        return contactos;  
     }
 
-    static agregarContacto(){
-
+    static agregarContacto(Contacto){
+        const contactos = Datos.traerContactos();
+        contactos.push(Contacto);
+        localStorage.setItem('contactos', JSON.stringify(contactos));
     }
 
-    static removerContact(){
+    static removerContacto(email){
+        const contactos = Datos.traerContactos();
 
+        contactos.forEach((contacto, index) => {
+            if (contacto.email === email){
+                contactos.splice(index, 1);
+            }
+        });
+
+        localStorage.setItem('contactos', JSON.stringify(contactos));
     }
 }
 
+document.addEventListener('DOMContentLoader', UI.mostrarContacto());
+
+
+document.querySelector('#contacto-form').addEventListener('submit', (e) =>{
+    e.preventDefault();
+
+    const nombre = document.querySelector('#nombre').value;
+    const telefono = document.querySelector('#telefono').value;
+    const direccion = document.querySelector('#direccion').value;
+    const email = document.querySelector('#email').value;
+
+    if (nombre === '' || telefono === '' || direccion === '' || email === ''){
+        UI.mostrarAlerta('Por favor ingrese todos los datos.', 'danger');
+    } else{
+        const contacto = new Contacto(nombre, direccion, telefono, email);
+        Datos.agregarContacto(contacto);
+        UI.agregarContactoLista(contacto);
+        UI.mostrarAlerta('Contacto agregado a la lista.', 'success');
+        UI.limpiarCampos();
+    }
+
+});
+
+document.querySelector('#contacto-list').addEventListener('click', (e) =>{
+    UI.eliminarContacto(e.target);
+    Datos.removerContacto(e.target.parentElement.previousElementSibling.textContent);
+    UI.mostrarAlerta('Contacto eliminado de la lista.', 'success');
+});
